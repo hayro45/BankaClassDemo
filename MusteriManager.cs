ce5a003c6abac5d62dad8f5dac5634d4c64f419a
Yaman <Yaman@DESKTOP-BN6B9N7>
2021-11-21 04:19:43 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı müşteri başarıyla eklendi.");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı müşteri başarıyla silindi.");
+        }
+
+        public void Listele(List<Musteri> musteriler) 
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Müşteriler ");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("{0}. Ad: {1} Soyad: {2} Bakiye: {3} TL",musteri.Id, musteri.Ad, musteri.Soyad, musteri.Bakiye);
+            }
+        }
+
+        public void ParaYukle(Musteri musteri, int para)
+        {
+            musteri.Bakiye += para;
+            Console.WriteLine("Başarıyla {0} adlı kişinin bakiyesine {1} TL yüklendi.", musteri.Ad, para);
+        }
+
+        public void ParaCek(Musteri musteri, int para)
+        {
+            musteri.Bakiye -= para;
+            Console.WriteLine("Başarıyla {0} adlı kişinin bakiyesinden {1} TL çekildi.", musteri.Ad, para);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Hayrettin";
+            musteri1.Soyad = "Dal";
+            musteri1.Bakiye = 123;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Murat";
+            musteri2.Soyad = "Dal";
+            musteri2.Bakiye = 213;
+
+            MusteriManager musteriManager = new MusteriManager();
+            
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            
+
+            List<Musteri> musteriler = new List<Musteri>() { musteri1, musteri2 };
+            musteriManager.Listele(musteriler);
+            
+            musteriManager.ParaYukle(musteri1, 54);
+            musteriManager.ParaCek(musteri2,23);
+          
+            musteriManager.Listele(musteriler);
+
+            musteriManager.Sil(musteri2);
+
+            
+        }
+    }
+}
